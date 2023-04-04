0fafe4fb0db7c07ea33888df30b3fd0af61dca01
ViggianoDevData <99290162+FeViggiano@users.noreply.github.com>
2023-04-04 00:18:55 -0300

    Add files via upload

---
+++ Prog_Banco_Apostila10_EX4.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION deter_sarrus(f INTEGER[][])
+
+RETURNS INTEGER AS $$
+
+
+DECLARE
+  deter_sarrus INTEGER;
+BEGIN
+  deter_sarrus := f[1][1] * f[2][2] * f[3][3] +
+         f[1][2] * f[2][3] * f[3][1] +
+         f[1][3] * f[2][1] * f[3][2] -
+         f[1][1] * f[2][3] * f[3][2] -
+         f[1][2] * f[2][1] * f[3][3] -
+         f[1][3] * f[2][2] * f[3][1];
+  RETURN deter_sarrus;
+
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT deter_sarrus(ARRAY[[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
